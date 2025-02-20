0e295548209ff728debf9ea33dd0f7ecef0f5248
Dmitri <272718+DimiG@users.noreply.github.com>
2025-02-20 16:03:11 +0300

    The BlinkRAW project has been added

---
+++ BlinkRAW/blink.s
@@ -0,0 +1,64 @@
+/**
+ * File Name : blink.s
+ * Author    : DG
+ * Version   : 1.0
+ * Date      : Feb 25, 2025
+ * About     : Assembler program to flash LED connected to the
+ *           : Raspberry Pi Pico GPIO (writing to the registers directly)
+ */
+
+  .global start
+
+  // Bare Metal Assembly Blinking Routine
+  // The internal LED is located on GPIO 25
+
+start:
+  // Releases the peripheral reset for iobank_0
+  ldr r0, =rst_clr    // Atomic register for clearing reset controller (0x4000c000+0x3000)
+  mov r1, #32         // Load a 1 into bit 5
+  str r1, [r0, #0]    // Store the bitmask into the atomic register to clear register
+
+rst:
+  // Check if reset is DONE
+  ldr r0, =rst_base   // Base address for reset controller
+  ldr r1, [r0, #8]    // Offset to get to the reset_done register
+  mov r2, #32         // Load 1 in bit 5 of register 2 (...0000000000100000)
+  and r1, r1, r2      // Isolate bit 5
+  beq rst             // If bit five is 0 then check again, if not, reset is done
+
+  // Set the control
+  ldr r0, =ctrl       // Control register for GPIO25
+  mov r1, #5          // Function 5, select SIO for GPIO25 (see RP2040 datasheet 2.19.2)
+  str r1, [r0]        // Store function_5 in GPIO25 control register
+
+  // Shifts over "1" the number of bits of GPIO pin
+  mov r1, #1          // Load a 1 into register 1
+  lsl r1, r1, #25     // Move the bit over to align with GPIO25
+  ldr r0, =sio_base   // SIO base
+  str r1, [r0, #36]   // 0x20 GPIO output enable
+
+led_loop:
+  // Blink loop
+  str r1, [r0, #20]   // 0x14 GPIO output value set
+  ldr r3, =big_num    // Load countdown number
+  bl delay            // Branch to subroutine delay
+
+  str r1, [r0, #24]   // 0x18 GPIO output value clear
+  ldr r3, =big_num    // Load countdown number
+  bl delay            // Branch to subroutine delay
+
+  b led_loop          // Do the loop again
+
+delay:
+  // Delay function
+  sub r3, #1          // Subtract 1 from register 3
+  bne delay           // Loop back to delay if not zero
+  bx lr               // Return from subroutine
+
+data:
+  .equ big_num, 0x00f00000    // Large number for the delay loop function
+  .equ ctrl, 0x400140cc       // GPIO25_CTRL (see RP2040 datasheet 2.19.6.1)
+  .equ rst_base, 0x4000c000   // Reset controller base (see RP2040 datasheet 2.14.3)
+  .equ rst_clr, 0x4000f000    // Atomic register for clearing reset controller (see RP2040 datasheet 2.1.2)
+  .equ sio_base, 0xd0000000   // SIO base (see RP2040 datasheet 2.3.1.7)
+
--- README.md
+++ README.md
@@ -6,18 +6,30 @@ Programs description
 --------------------
 `HelloWorld`: This is an example of a **Hello World** program written in assembly language using the SDK. In order for this example to work, it is necessary:
 1. Set an environment variable specifying the location of the SDK on your computer:  
-`export PICO_SDK_PATH="$HOME/pico/pico-sdk"`;
-2. To compile it run from project directory - `cmake -S . -B build && cmake --build build`;
+`export PICO_SDK_PATH="$HOME/pico/pico-sdk"`
+2. To compile it run from project directory - `cmake -S . -B build && cmake --build build`
 3. To load firmware into microcontroller use such command:  
-`picotool load build/app_name.uf2; picotool reboot`;
+`picotool load build/HelloWorld.uf2; picotool reboot`
 4. To test the program start the Raspberry Pi Pico with boot button pressed and run:  
 `tio /dev/ttyACM0` (ctrl-t q for exit)
 
+`BlinkRAW`: This is an example of a **Blink LED** program written in assembly language and using the SDK.  
+The control is performed by direct writing to the registers of the microcontroller.  
+The `main.c` is used as an example of integrating the code written in `Assembler` with `C` code.  
+In order for this example to work, it is necessary:
+1. Set an environment variable specifying the location of the SDK on your computer:  
+`export PICO_SDK_PATH="$HOME/pico/pico-sdk"`
+2. Prepare the CMake environment by: `cmake -S . -B build`
+3. Compile the code by: `cmake --build build`
+4. Load **FIRMWARE** into microcontroller by: `picotool load build/BlinkRAW.uf2`
+5. Reboot microcontroller by: `picotool reboot`
+6. Watch the blink result
+
 `All`: **NOTE**
-* For CMake based projects to clean the project target use - `cmake --build build --target clean`;
-* Picotool official utility located **[HERE][picotool]**;
-* Powerful `tio` program located **[HERE][tio]**;
-* Raspberry Pi Pico SDK located **[HERE][picosdk]**.  
+* For CMake based projects to clean the project target use - `cmake --build build --target clean`
+* Picotool official utility is located **[HERE][picotool]**
+* Powerful `tio` program is located **[HERE][tio]**
+* Raspberry Pi Pico SDK is located **[HERE][picosdk]**  
 
 ***Requires :*** [Pico SDK][picosdk] preinstalled.
 
